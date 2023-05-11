eb224796dcb248d3776f79d92449fb524e714116
gastondonikian <gastondonikian@gmail.com>
2023-05-10 21:37:29 -0300

    Menu and Victory. Missing sound!

---
+++ Assets/Scripts/Managers/EndGameManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GlobalScripts;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class EndGameManager : MonoBehaviour
+    {
+        [SerializeField] private Sprite _victory;
+        [SerializeField] private Sprite _defeat;
+        [SerializeField] private Image _background;
+        private void Start()
+        {
+            _background.sprite = GlobalVictory.instance.IsVictory ? _victory : _defeat;
+        }
+
+        public void ActionMainMenu() => SceneManager.LoadScene(UnityScenes.Menu.ToString());
+    }
+}
---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum UnityScenes
+{
+    [Description("Main Menu")]
+    Menu,
+    [Description("Level1")]
+    Level1,
+    [Description("EndGame")]
+    EndGame,
+        
+}
+namespace Managers
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public void LoadLevelOne() => SceneManager.LoadScene(UnityScenes.Level1.ToString());
+
+        public void Quit() => Application.Quit();
+    }
+}
