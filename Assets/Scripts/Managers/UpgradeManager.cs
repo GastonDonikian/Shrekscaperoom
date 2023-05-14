317b768e113ccdd9ebb335750debbce0840a35e9
gastondonikian <gastondonikian@gmail.com>
2023-05-14 15:58:35 -0300

    UpgradeManager and GlobalUpgrades

---
+++ Assets/Scripts/Managers/UpgradeManager.cs
@@ -0,0 +1,27 @@
+﻿using GlobalScripts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class UpgradeManager: MonoBehaviour
+    {
+        private GlobalUpgrades _globalUpgrades;
+        private void Start()
+        {
+            this._globalUpgrades = GlobalUpgrades.instance;
+        }
+
+        public void ActionUpgradeSpeed()
+        {
+            _globalUpgrades.IncreaseSpeed();
+            SceneManager.LoadScene(UnityScenes.Level1.ToString());
+        }
+        
+        public void ActionUpgradePower()
+        {
+            _globalUpgrades.IncreasePower();
+            SceneManager.LoadScene(UnityScenes.Level1.ToString());
+        }
+    }
+}
