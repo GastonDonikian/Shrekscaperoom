39ab94adb685a8b797d661a9f1c68610a87735fc
gastondonikian <gastondonikian@gmail.com>
2023-05-16 19:38:32 -0300

    Sound effects

--- Assets/Scripts/Actors/Characters/Shrek.cs
+++ Assets/Scripts/Actors/Characters/Shrek.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sounds;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,8 +11,10 @@ public class Shrek : Actor
     public Transform Target;
 
     private MovementController _movementController;
+    private SoundDamageEffectController _soundDamageEffectController;
     private void Start()
     {
+        _soundDamageEffectController = GetComponent<SoundDamageEffectController>();
         _shrek = GetComponent<NavMeshAgent>();
     }
 
@@ -28,6 +31,7 @@ public class Shrek : Actor
         //if collided with character give character damage
         if (collision.gameObject.layer == 6)
         {
+            _soundDamageEffectController.OnDamage();
             var damageable = collision.gameObject.GetComponent<IDamageable>();
             if (damageable != null) EventQueueManager.instance.AddEvent(new CmdApplyDamage(damageable, 10000));
             
---
+++ Assets/Scripts/Sounds/SoundDamageEffectController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+namespace Sounds
+{
+    public class SoundDamageEffectController : MonoBehaviour
+    {
+        [SerializeField] private List<AudioClip> _damages;
+        private int _index;
+        public AudioSource AudioSource => _audioSource;
+        private AudioSource _audioSource;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _index = 0;
+            _audioSource = GetComponent<AudioSource>();
+        }
+    
+        public void OnDamage()
+        {
+            _audioSource.PlayOneShot(_damages[_index]);
+            _index = (_index + 1) % _damages.Count;
+        }
+    }
+}
