037e42b4c35046199a643df3cce81688ff93e2d4
sespina99 <sespina@itba.edu.ar>
2023-06-17 11:11:38 -0300

    new shrek and donkey and grenade

---
+++ Assets/Scripts/Actors/Grenade/GranadeThrowController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GranadeThrowController : MonoBehaviour
+{
+    [SerializeField] private float _throwForce = 50f;
+    [SerializeField] private GameObject _granadePrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+         ThrowGranade();   
+        }
+    }
+
+    private void ThrowGranade()
+    {
+        //spawn de granada
+        GameObject granade = Instantiate(_granadePrefab, transform.position + transform.forward, transform.rotation);
+        //acceso al rigidbody
+        Rigidbody rb = granade.GetComponent<Rigidbody>();
+        //aplicar fuerza
+        rb.AddForce(transform.forward * _throwForce, ForceMode.VelocityChange);
+    }
+}
