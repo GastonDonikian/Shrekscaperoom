ec34f57def68e91d02defed33b6329f9d18db75b
gastondonikian <gastondonikian@gmail.com>
2023-06-24 22:47:44 -0300

    Added load async

---
+++ Assets/Scripts/Managers/LoadAsyncManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class LoadAsyncManager : MonoBehaviour
+    {
+        [SerializeField] private Image _progressBar;
+        [SerializeField] private string _targetScene = UnityScenes.Level1.ToString();
+
+
+        private void Start()
+        {
+            StartCoroutine(LoadAsync());
+            
+        }
+        IEnumerator LoadAsync()
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(_targetScene);
+            float progress = 0;
+
+            while (!operation.isDone)
+            {
+                progress = operation.progress;
+                _progressBar.fillAmount = progress;
+
+                yield return null;
+            }
+        }
+
+    }
+}
