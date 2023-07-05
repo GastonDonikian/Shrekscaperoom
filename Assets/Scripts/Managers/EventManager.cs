3c054df3eb6fc28581668a1a5eaf9e9423bec192
sespina99 <sespina@itba.edu.ar>
2023-07-04 23:25:29 -0300

    game logic, from main menu to lvl1 to lvl 2 to endgame

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -16,6 +16,8 @@ public class EventManager : MonoBehaviour
     public event Action<int> ActionCharacterLifeChange;
     public event Action<int> ActionCharacterSpeedChange;
     public event Action<bool> OnGameOver;
+
+    public event Action<bool> OnLvl2Over;
     public event Action<bool> OnChase;
 
     public event Action<int, int> ActionOnWeaponFired;
@@ -29,6 +31,8 @@ public class EventManager : MonoBehaviour
     }
 
     public void ActionGameOver(bool isGameOver) => OnGameOver(isGameOver);
+    
+    public void ActionLvl2Over(bool isGameOver) => OnLvl2Over(isGameOver);
     public void StartChase(bool isDoorBroken) => OnChase(isDoorBroken);
 
     public void OnCharacterSpeedChange(int currentSpeed) => ActionCharacterSpeedChange(currentSpeed);
