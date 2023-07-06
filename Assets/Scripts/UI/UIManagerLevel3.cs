ca5ca25f8d84c2b25d13063b14f16cca39fb0d0a
gastondonikian <gastondonikian@gmail.com>
2023-07-06 20:48:05 -0300

    Settings and HUD level 3

---
+++ Assets/Scripts/UI/UIManagerLevel3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GlobalScripts;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class UIManagerLevel3 : MonoBehaviour
+    {
+        [SerializeField] private List<Image> _lifeIcon;
+        [SerializeField] private Text _ammoValue;
+        [SerializeField] private Image _transitionImage;
+
+        private void Start()
+        {
+            var tempColor = _transitionImage.color;
+            tempColor.a = 1f;
+            _transitionImage.color = tempColor;
+
+            _ammoValue.text = "1/1";
+            OnCharacterLifeChange(GlobalUpgrades.instance.lives);
+            EventManager.instance.ActionOnWeaponFired += OnWeaponFired;
+            StartCoroutine("ChangeStartingOverlay");
+        }
+
+        IEnumerator ChangeStartingOverlay()
+        {
+            while(_transitionImage.color.a > 0){
+                var tempColor = _transitionImage.color;
+                tempColor.a -= 0.005f;
+                _transitionImage.color = tempColor;
+                yield return null;
+            }
+        }
+
+        private void OnWeaponFired(int currentAmmo, int maxAmmo)
+        {
+            _ammoValue.text = currentAmmo + "/" + maxAmmo;
+        }
+        private void OnCharacterLifeChange(int currentLife)
+        {
+            var i = 0;
+            foreach (var listIcon in _lifeIcon)
+            {
+                if (i < currentLife)
+                {
+                    listIcon.enabled = true;
+                    listIcon.GetComponentInChildren<Image>().enabled = true;
+                }
+                else
+                {
+                    listIcon.GameObject().SetActive(false);
+                }
+                i++;
+            }
+        }
+    }
+}
