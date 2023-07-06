8082244416fe4e0b84a035dcbc1442102ece5498
sespinaItba <sespinaItba@itba.edu.ar>
2023-07-06 02:00:56 -0300

    falling floor logic

--- Assets/Scripts/Actors/Map/ShrekBlock.cs
+++ Assets/Scripts/Actors/Map/ShrekBlock.cs
@@ -8,9 +8,12 @@ public class ShrekBlock : MonoBehaviour
     private Transform _target;
     void Update()
     {
-        Vector3 direction = _target.position - transform.position;
-        direction.y = 1;
-        transform.rotation = Quaternion.LookRotation(direction);
+        if (_target != null)
+        {
+            Vector3 direction = _target.position - transform.position;
+            direction.y = 1;
+            transform.rotation = Quaternion.LookRotation(direction);   
+        }
     }
 
     private void Start()
